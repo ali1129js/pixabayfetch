16bd2d4656a8792185aff6d2951c9c81bd384a86
ali1129js <ali1129js@gmail.com>
2019-09-17 15:31:15 +0200

    Updates

---
+++ precache-manifest.cb29ec2946a5b70d99f5abfabead47ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "daa202e31af49b00a5e3",
+    "url": "/pixaBayFetchLive/static/js/runtime~main.daa202e3.js"
+  },
+  {
+    "revision": "3cd57b14131b714c9359",
+    "url": "/pixaBayFetchLive/static/js/main.3cd57b14.chunk.js"
+  },
+  {
+    "revision": "4e2972bab64b83944fe9",
+    "url": "/pixaBayFetchLive/static/js/1.4e2972ba.chunk.js"
+  },
+  {
+    "revision": "3cd57b14131b714c9359",
+    "url": "/pixaBayFetchLive/static/css/main.9c2b12ce.chunk.css"
+  },
+  {
+    "revision": "4e2972bab64b83944fe9",
+    "url": "/pixaBayFetchLive/static/css/1.68555509.chunk.css"
+  },
+  {
+    "revision": "ec678770392d2020823ab171cb93cf91",
+    "url": "/pixaBayFetchLive/index.html"
+  }
+];
